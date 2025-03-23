713a3b6d1a5ceebf34b07a253c860dbd81667814
Daniil <daniillapyshov@yandex.ru>
2025-03-23 18:53:41 +0300

    Файл /WebViewPresenterSpy.swift

---
+++ ImageFeedTests/WebViewTests/WebViewPresenterSpy.swift
@@ -0,0 +1,22 @@
+//
+//  WebViewPresenterSpy.swift
+//  ImageFeed
+//
+//  Created by Даниил Лапышов on 22.03.2025.
+//
+import ImageFeed
+import Foundation
+
+final class WebViewPresenterSpy: WebViewPresenterProtocol {
+    var viewDidLoadCalled: Bool = false
+    var view: WebViewViewControllerProtocol?
+    
+    func viewDidLoad() {
+        viewDidLoadCalled = true
+    }
+    func didUpdateProgressValue(_ newValue: Double) {
+    }
+    func code(from url: URL) -> String? {
+        return nil
+    }
+}
